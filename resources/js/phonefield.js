ffb9afa1243da864df773899bab5dcf4a171bd39
Serdar Cakir <serdarekremcakir@hotmail.com>
2023-10-12 12:33:09 +0300

    added allowed and default country to masked inputs

--- resources/js/phonefield.js
+++ resources/js/phonefield.js
@@ -1,4 +1,10 @@
 function phoneMask(fields) {
+    if (typeof (allowed_cities) != "undefined") {
+        var onlyCountriesOption = allowed_cities ? {onlyCountries: allowed_cities} : {};
+    }
+
+    var initialCountry = (typeof country_default !== "undefined") ? country_default : 'auto';
+
     var country = document.getElementById('default-phone-country')
         ? document.getElementById('default-phone-country').innerText : null;
     var inputQueries = document.querySelectorAll(fields);
@@ -8,26 +14,27 @@ function phoneMask(fields) {
             class: "form-control",
             formatOnDisplay: true,
             nationalMode: true,
-            initialCountry: "auto",
             geoIpLookup: function (success, failure) {
                 $.get("https://ipinfo.io", function () {
                 }, "jsonp").always(function (resp) {
                     var countryCode = country ? country : (resp && resp.country) ? resp.country : "";
                     success(countryCode);
                 })
-            }
+            },
+            ...onlyCountriesOption,
+            initialCountry
         });
 
-        $(inputQuery).on("countrychange", function(event) {
+        $(inputQuery).on("countrychange", function (event) {
             iti.setNumber("");
             addMask(iti, inputQuery);
         });
     });
 
-    function addMask(iti, inputQuery){
+    function addMask(iti, inputQuery) {
         let selectedCountryData = iti.getSelectedCountryData();
         let newPlaceholder = intlTelInputUtils.getExampleNumber(selectedCountryData.iso2, true, intlTelInputUtils.numberFormat.INTERNATIONAL);
-        $(inputQuery).inputmask({ mask: newPlaceholder.replace(/[0-9+]/ig,'9'), keepStatic: false });
+        $(inputQuery).inputmask({mask: newPlaceholder.replace(/[0-9+]/ig, '9'), keepStatic: false});
     }
 
     // var fields_arr = fields.split(',');
